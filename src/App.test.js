15eb494a71104160fed1b994a42262cc9ed01393
fteve96 <ftevenden1@gmail.com>
2020-05-28 14:47:54 -0700

    intial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('app.js', () => {
+  it('renders without crashing', () => {
+    let div = document.createElement('div')
+    ReactDOM.render(
+      <App />,
+      div
+    )
+  })
+}) 
+
