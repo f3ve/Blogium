2708262ad2ed0f49a562b12d124ffa42152aaba8
fteve96 <ftevenden1@gmail.com>
2020-05-29 14:02:53 -0700

    setting up dummy-store

---
+++ src/dummy-store.js
@@ -0,0 +1,29 @@
+export default {
+  users: [
+    {
+      id: 1,
+      full_name: 'Jimmy Timmy',
+      username: 'jimbo',
+      email: 'jimmy@email.com',
+      password: 'password'
+    },
+    {
+      id: 2,
+      full_name: 'Jamie Dude',
+      username: 'Jamo',
+      email: 'jamie@email.com',
+      password: 'password'
+    },
+    {
+      id: 3,
+      full_name: 'Josh McJosh',
+      username: 'JoshyWashy',
+      email: 'josh@email.com',
+      password: 'password'
+    }
+  ],
+
+  posts: [
+    
+  ]
+}
