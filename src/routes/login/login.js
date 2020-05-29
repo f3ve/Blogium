27f526ddb40f2059cd171c7e6ffb38917c97e6b0
fteve96 <ftevenden1@gmail.com>
2020-05-29 13:50:09 -0700

    implemented login route

--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
+import './login.css'
 
 class Login extends React.Component {
+
+  clickCancel(e) {
+    e.preventDefault()
+    this.props.history.push('/')
+  }
+
   render() {
     return(
-      <h1>hey</h1>
+      <section id='login-container'>
+        <h2>Login</h2>
+        <form id='login-form'>
+          <label htmlFor='username'>Username:</label>
+          <input 
+            type='text'
+            id='username'
+            required
+          />
+
+          <label htmlFor='password'>Password:</label>
+          <input 
+            type='password'
+            id='password'
+            required
+          />
+          <div className='button-container'>
+            <button>Login</button>
+            <button onClick={e => this.clickCancel(e)}>Cancel</button>
+          </div>
+        </form>
+      </section>
     )
   }
 }
