613703917884da4b73cc7d7fa95169303708d75a
fteve96 <ftevenden1@gmail.com>
2020-06-03 00:49:29 -0700

    polished the editor

--- src/components/editorToolbar/editorToolbar.js
+++ src/components/editorToolbar/editorToolbar.js
@@ -3,14 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './editorToolbar.css'
 
 export default function EditorToolbar(props) {
-  const { format, setUrl, handleSubmit } = props
+  const { format, setUrl, handleSubmit, addLink, setTitle, addCodeBlock } = props
   return (
     <div class="sample-toolbar">
       <a href="javascript:void(0)" onClick={e => format('bold')}><FontAwesomeIcon icon='bold' className='icon' /></a>
       <a href="javascript:void(0)" onClick={e => format('italic')}><FontAwesomeIcon icon='italic' className='icon'/></a>
       <a href="javascript:void(0)" onClick={e => format('insertUnorderedList')}><FontAwesomeIcon icon='list' className='icon'/></a>
-      <a href="javascript:void(0)" onClick={e => setUrl()}><FontAwesomeIcon icon='link' className='icon' /></a>
-      <span><input id="txtFormatUrl" placeholder="url" class="form-control" /></span>
+      <a href="javascript:void(0)" onClick={e => addLink()}><FontAwesomeIcon icon='link' className='icon' /></a>
+      <div id='url-input' className='hidden'>
+        <input id="txtFormatUrl" placeholder="url" class="form-control" />
+        <button onClick={e => setUrl(e)}>Create Link</button>
+      </div>
+      <a href="javascript:void(0)" onClick={e => setTitle()}><FontAwesomeIcon icon='heading' className='icon' /></a>
+      <a href="javascript:void(0)" onClick={e => addCodeBlock()}><FontAwesomeIcon icon='code' className='icon' /></a>
       <a href="javascript:void(0)" onClick={e => handleSubmit()}><FontAwesomeIcon icon='plus' className='icon'/></a>
     </div>
   )
--- src/routes/editor/editor.js
+++ src/routes/editor/editor.js
@@ -7,11 +7,66 @@ export default class Editor extends React.Component {
     document.execCommand(command, false, value);
   }
   
-  setUrl = () => {
+  setUrl = (e) => {
+    e.preventDefault()
     const url = document.getElementById('txtFormatUrl').value;
+    const show = document.getElementById('url-input')
     const sText = document.getSelection();
     document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
-    document.getElementById('txtFormatUrl').value = '';
+    document.getElementById('txtFormatUrl').value = ''
+    show.classList.add('hidden')
+  }
+
+  addLink = () => {
+    const show = document.getElementById('url-input')
+    if (show.classList.contains('hidden')) {
+      show.classList.remove('hidden')
+    } else {
+      show.classList.add('hidden')
+    }
+  }
+
+  setTitle = () => {
+    const target = document.getSelection();
+    document.execCommand('insertHTML', false, '<h2>' + target + '</h2>')
+  }
+
+  addCodeBlock = () => {
+    const codeBlock = document.createElement('pre')
+    const target = document.getSelection()
+    if (target.focusNode.nodeName.includes('#text') || target.focusNode.classList.contains('title') || target.focusNode.className.includes('codeBlock')) {
+      return
+    }
+    const id = `codeBlock-${document.getElementsByClassName('codeBlock').length + 1}`
+
+    codeBlock.classList.add('codeBlock')
+    codeBlock.addEventListener('paste', this.paste)
+    document.execCommand('insertHTML', false, `<pre class='codeBlock' id='${id}'>${target}</pre>`)
+    
+    this.addLineAfterBlock(id)
+  }
+
+  addLineAfterBlock(id) {
+    const block = document.getElementById(`${id}`)
+    const div = document.createElement('div')
+    const br = document.createElement('br')
+    div.appendChild(br)
+    
+    if (!block) {
+      return
+    } else {
+      block.after(div)
+    }
+  }
+
+  paste = (e) => {
+    e.preventDefault()
+
+    const open = new RegExp('<', 'gi')
+    const close = new RegExp('>', 'gi')
+    const text = (e.originalEvent || e).clipboardData.getData('text/plain').replace(open, '&lt').replace(close, '&gt')
+
+    document.execCommand('insertHTML', false, text)
   }
   
   handleSubmit = () => {
@@ -37,15 +92,19 @@ export default class Editor extends React.Component {
   }
 
   render() {
-    // window.addEventListener('load', function(){
-    //   document.getElementById('sampleeditor').setAttribute('contenteditable', 'true');
-    //   document.getElementById('sampleeditor2').setAttribute('contenteditable', 'true');
-    // });
     return (
       <React.Fragment>
-        <EditorToolbar setUrl={this.setUrl} format={this.format} handleSubmit={this.handleSubmit} />
-        <div className='editor' id='sampleeditor' contentEditable='true'>
-          <h2 id='title'>Enter Your Title...</h2>
+        <EditorToolbar 
+          setUrl={this.setUrl} 
+          format={this.format} 
+          addLink={this.addLink}
+          handleSubmit={this.handleSubmit} 
+          setTitle={this.setTitle}
+          addCodeBlock={this.addCodeBlock}
+        />
+        <div id='title' contentEditable='true' spellCheck='true' data-placeholder='Title...' className='title'>
+        </div>
+        <div className='editor' id='sampleeditor' contentEditable='true' spellCheck='true' data-placeholder='Body...' onPaste={this.paste}>
         </div>
       </React.Fragment>
     )
