d8a3242edea426b941e4062e5305476ca7707b38
fteve96 <ftevenden1@gmail.com>
2020-06-19 04:43:41 -0700

    updated tests for recent changes

--- src/routes/drafts/drafts.test.js
+++ src/routes/drafts/drafts.test.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter } from 'react-router-dom'
 import Drafts from './drafts'
 
 describe('drafts route', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
-      <Drafts />,
+      <BrowserRouter>
+       <Drafts />
+      </BrowserRouter>,
       div
     )
     ReactDOM.unmountComponentAtNode(div)
--- src/routes/userPage/userPage.test.js
+++ src/routes/userPage/userPage.test.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
 import UserPage from './userPage'
 
 describe('UserPage route', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
-      <UserPage />,
+      <BrowserRouter>
+        <UserPage />
+      </BrowserRouter>,
       div
     )
   })
